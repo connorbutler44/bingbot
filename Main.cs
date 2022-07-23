11173c8eb56003080bc78871bed0efd2a39bb843
Connor Butler <connor.butler44@gmail.com>
2022-07-23 02:24:21 -0400

    Pull voice codes from gist

--- Main.cs
+++ Main.cs
@@ -7,6 +7,8 @@ using System.Threading.Tasks;
 using Discord;
 using Discord.WebSocket;
 using Newtonsoft.Json.Linq;
+using System.Net.Http;
+using System.Text.Json;
 
 namespace Bingbot
 {
@@ -15,6 +17,9 @@ namespace Bingbot
         private readonly DiscordSocketClient _client;
         private readonly TextToSpeechService _ttsService;
         private readonly ulong DM_CHANNEL_ID = 688040246499475525;
+        private readonly string VOICE_CODES_URL = "https://gist.githubusercontent.com/connorbutler44/118d8c69e42de0113cd629fc5985b625/raw/e09f33b52829a75286aa76ba0101718717c2949e/gistfile1.txt";
+
+        private Dictionary<string, string> emoteDictionary = new Dictionary<string, string>();
 
 
         // Discord.Net heavily utilizes TAP for async, so we create
@@ -38,12 +43,13 @@ namespace Bingbot
             _ttsService = new TextToSpeechService();
         }
     
-            public async Task MainAsync()
+        public async Task MainAsync()
         {
             JObject env = JObject.Parse(File.ReadAllText(@"./.env.json"));
 
             await _client.LoginAsync(TokenType.Bot, env.GetValue("API_KEY").ToString());
             await _client.StartAsync();
+            await RefreshEmoteDictionary();
 
             // Block the program until it is closed.
             await Task.Delay(Timeout.Infinite);
@@ -70,6 +76,12 @@ namespace Bingbot
             if (message.Author.Id == _client.CurrentUser.Id)
                 return;
 
+            if (message.Content == "!emoterefresh")
+            {
+                await RefreshEmoteDictionary();
+                await message.Channel.SendMessageAsync("Done 👍");
+            }
+
             // any DM's the bot recieves will send the TTS to a specific channel
             // if (message.Channel.GetChannelType() == ChannelType.DM && message.Content.Trim().Length > 0)
             // {
@@ -94,23 +106,6 @@ namespace Bingbot
             }
         }
 
-        private static readonly Dictionary<string, string> emoteDictionary = new Dictionary<string, string>()
-            {
-                { "🐻", Voice.Chewbacca },
-                { "🤖", Voice.C3PO },
-                { "🦝", Voice.Rocket },
-                { "jarrad", Voice.AusMale },
-                { "a", Voice.UkMale },
-                { "👩", Voice.UsFemale },
-                { "BOGGED", Voice.FrenchMale },
-                { "🇩🇪", Voice.GermanMale },
-                { "c", Voice.SpanishMale },
-                { "d", Voice.JpFemale },
-                { "e", Voice.KoreanMale },
-                { "🎵", Voice.AltoFemale },
-                { "z", Voice.TenorMale },
-            };
-
         private string GetVoice(IEnumerable<IEmote> reactions)
         {
             foreach(IEmote reaction in reactions)
@@ -121,6 +116,27 @@ namespace Bingbot
 
             // fallback to UsFemale if no valid reaction is found
             return Voice.UsFemale;
+        }
+
+        /*
+            Emote voice mappings are stored in a gist so we can easily update them without committing/redeploying the bot
+        */
+        private async Task RefreshEmoteDictionary()
+        {
+            Console.WriteLine("Refreshing Emote Dictionary");
+
+            HttpClient client = new HttpClient();
+            Stream response = await client.GetStreamAsync(VOICE_CODES_URL);
+            var voiceCodes = JsonSerializer.Deserialize<List<VoiceCode>>(response);
+
+            var emoteDict = new Dictionary<string, string>();
+
+            foreach(var voiceCode in voiceCodes)
+            {
+                emoteDict.Add(voiceCode.emote, voiceCode.code);
+            }
+
+            this.emoteDictionary = emoteDict;
         }
     }
 }
---
+++ Model/VoiceCode.cs
@@ -0,0 +1,6 @@
+public class VoiceCode
+{
+    public string description { get; set; }
+    public string code { get; set; }
+    public string emote { get; set; }
+}

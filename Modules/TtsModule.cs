e17ab632317b19719e2cd08019be5e1b2130c631
Connor Butler <connor.butler44@gmail.com>
2023-04-14 23:08:50 -0400

    Oblivion guard

--- Modules/TtsModule.cs
+++ Modules/TtsModule.cs
@@ -25,7 +25,7 @@ namespace Bingbot.Modules
                 Choice("Asher", "QBaMLEOzUYxyKKh7ZuZN"), Choice("Todd Howard", "8oLT3oOTUp9RV6GHl7AU"),
                 Choice("Whopper", "Zuzo46BJSET6252mCZX5"), Choice("Tim Gunn", "lQV6YBaetZO5fb2n1JSV"),
                 Choice("Halo Announcer", "2mY0k5zCDvLApJhuUvS4"), Choice("Cortana", "G5JvFs8Ivxbn4PsafONN"),
-                Choice("Chills", "XmxA2cgmSD8ydzjAjP7G")] string voice,
+                Choice("Chills", "XmxA2cgmSD8ydzjAjP7G"), Choice("Oblivion Guard", "S4MX3ES8njsedM3zBZhJ")] string voice,
             [Summary(description: "Text to be used for tts")]
             string text,
             [Summary(description: "0-100. Higher values: consistency + monotonality. Lower values: More expressive + instability.")]

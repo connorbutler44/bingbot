2d8fae85fc11fdc8cfd14f834eef96350d380599
Connor Butler <connor.butler44@gmail.com>
2023-08-02 23:17:50 -0400

    Add new voices

--- Modules/TtsModule.cs
+++ Modules/TtsModule.cs
@@ -25,7 +25,9 @@ namespace Bingbot.Modules
                 Choice("Asher", "QBaMLEOzUYxyKKh7ZuZN"), Choice("Todd Howard", "8oLT3oOTUp9RV6GHl7AU"),
                 Choice("Whopper", "Zuzo46BJSET6252mCZX5"), Choice("Tim Gunn", "lQV6YBaetZO5fb2n1JSV"),
                 Choice("Halo Announcer", "2mY0k5zCDvLApJhuUvS4"), Choice("Cortana", "G5JvFs8Ivxbn4PsafONN"),
-                Choice("Chills", "XmxA2cgmSD8ydzjAjP7G"), Choice("Oblivion Guard", "S4MX3ES8njsedM3zBZhJ")] string voice,
+                Choice("Chills", "XmxA2cgmSD8ydzjAjP7G"), Choice("Oblivion Guard", "S4MX3ES8njsedM3zBZhJ"),
+                Choice("Girly", "EHEP5eqnatUfacCbMlOR"), Choice("Richard Mealey", "KRHlgM8XFdAaiKgbyzx9"),
+                Choice("Deep Voice British Mans", "4OBaNu67U8XfuTFNHrsm")] string voice,
             [Summary(description: "Text to be used for tts")]
             string text,
             [Summary(description: "0-100. Higher values: consistency + monotonality. Lower values: More expressive + instability.")]

3b8fbbd14daddd719ec9bc350eb938ff0c99bccb
Connor Butler <connor.butler44@gmail.com>
2023-11-30 21:40:30 -0500

    Add log stats

--- Model/UserLog.cs
+++ Model/UserLog.cs
@@ -5,5 +5,5 @@ public class UserLog
     public Guid Id { get; set; }
     public ulong UserId { get; set; }
     public ulong GuildId { get; set; }
-    public DateTime TakenAt { get; set; }
+    public DateTimeOffset TakenAt { get; set; }
 }
--- Modules/StatisticsModule.cs
+++ Modules/StatisticsModule.cs
@@ -1,6 +1,10 @@
 using System.Threading.Tasks;
 using Discord.Interactions;
 using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Discord;
+using System.Collections.Generic;
 
 namespace Bingbot.Modules
 {
@@ -18,7 +22,7 @@ namespace Bingbot.Modules
         }
 
         [SlashCommand("log", "Dropped a log", runMode: RunMode.Async)]
-        public async Task TextToSpeech(
+        public async Task Log(
             [Summary(description: "The shit will be logged (haha), but Bingbot won't respond in a way that's public to the server")]
             bool suppressResponse = false)
         {
@@ -32,6 +36,65 @@ namespace Bingbot.Modules
             await _db.SaveChangesAsync();
 
             await RespondAsync("<:shittin:1135740341182529747>", ephemeral: suppressResponse);
+        }
+
+        [SlashCommand("stats", "Log stats!", runMode: RunMode.Async)]
+        public async Task Stats()
+        {
+            // total number of logs for this user
+            var total = (
+                await _db.UserLogs
+                    .Where(x => x.UserId == Context.User.Id && x.GuildId == Context.Guild.Id)
+                    .ToListAsync()
+            ).Count;
+
+            // number of logs for this user this week
+            var startOfWeek = DateTimeOffset.Now.AddDays(-(int)DateTimeOffset.Now.DayOfWeek + (int)DayOfWeek.Monday).UtcDateTime;
+
+            var thisWeekTotal = (
+                await _db.UserLogs
+                    .Where(x => x.TakenAt >= startOfWeek && x.UserId == Context.User.Id && x.GuildId == Context.Guild.Id)
+                    .ToListAsync()
+            ).Count;
+
+            // most logs in a singular day
+            var mostInDay = (
+                await _db.UserLogs
+                    .Where(x => x.UserId == Context.User.Id && x.GuildId == Context.Guild.Id)
+                    .GroupBy(x => x.TakenAt.Date)
+                    .Select(x => new { Date = x.Key, Count = x.Count() })
+                    .OrderByDescending(x => x.Count)
+                    .FirstOrDefaultAsync()
+            ).Count;
+
+            var embed = new EmbedBuilder
+            {
+                Color = new Color(191, 105, 82),
+                Title = $"{Context.User.Username}'s 💩 stats",
+                Fields = new List<EmbedFieldBuilder>
+                {
+                    new EmbedFieldBuilder
+                    {
+                        Name = "Total",
+                        Value = total,
+                        IsInline = true
+                    },
+                    new EmbedFieldBuilder
+                    {
+                        Name = "This week",
+                        Value = thisWeekTotal,
+                        IsInline = true
+                    },
+                    new EmbedFieldBuilder
+                    {
+                        Name = "Most in a day",
+                        Value = mostInDay,
+                        IsInline = true
+                    }
+                },
+            };
+
+            await RespondAsync(embed: embed.Build());
         }
     }
 }
